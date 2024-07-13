24c9d4c6e0ecef8cdfed2610ea318979eb1f5f2b
Aman Kadam <126617073+AmanKadam-16@users.noreply.github.com>
2024-07-13 14:37:28 +0000

    boiler code for React | Folder Structure

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+
+
+function App(){
+
+// logical content
+
+let name = "Code_RED"
+
+
+  return(
+// Rendering Content
+
+<h1 id="name">Hello World..!  {name}</h1>
+
+
+
+
+
+
+  )
+}
+
+export default App
